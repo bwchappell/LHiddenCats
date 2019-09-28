b80bccabdceebcb3d0d23838470d36296746acf8
Benjamin Chappell <bwchappell@gmail.com>
2019-09-28 16:18:13 -0600

    With gradient checking

--- LHiddenCats.py
+++ LHiddenCats.py
@@ -1,5 +1,6 @@
 import numpy as np
 import h5py
+import copy
 
 
 class DataSet:
@@ -34,8 +35,8 @@ class NeuralNet:
     def initialize_parameters(self, n_x: int):
         np.random.seed(1)
         L = len(self.neuron_dims)
-        self.W = [None, ]
-        self.b = [None, ]
+        W = [None, ]
+        b = [None, ]
         neuron_dim_list = (n_x,) + self.neuron_dims
 
         if self.mid_activation == "relu":
@@ -44,9 +45,11 @@ class NeuralNet:
             adj = 1
 
         for l in range(1, L+1):
-            self.W.append(np.random.randn(neuron_dim_list[l-1], neuron_dim_list[l]) * np.sqrt(adj/neuron_dim_list[l-1]))
+            W.append(np.random.randn(neuron_dim_list[l-1], neuron_dim_list[l]) * np.sqrt(adj/neuron_dim_list[l-1]))
             #self.W.append(np.random.randn(neuron_dim_list[l], neuron_dim_list[l-1]).T / np.sqrt(neuron_dim_list[l-1])) #this one will produce the same results as Coursera
-            self.b.append(np.zeros((1, neuron_dim_list[l])))
+            b.append(np.zeros((1, neuron_dim_list[l])))
+
+        return W, b
 
     @staticmethod
     def sigmoid(z: np.ndarray):
@@ -67,7 +70,7 @@ class NeuralNet:
         # Should add an exception if the activation isn't an anticipated value
         return A
 
-    def forward_prop(self, data: DataSet):
+    def forward_prop(self, data: DataSet, W, b):
         L = len(self.neuron_dims)
         X, Y = data.get_data()
         Z = [None, ]
@@ -78,7 +81,7 @@ class NeuralNet:
             D = [np.ones(X.shape), ]
 
         for l in range(1, L+1):
-            Z.append(np.dot(A[l-1], self.W[l]) + self.b[l])
+            Z.append(np.dot(A[l-1], W[l]) + b[l])
             if l == L:
                 A.append(NeuralNet.activate(self.final_activation, Z[l]))
 
@@ -92,17 +95,17 @@ class NeuralNet:
                     D[l] = D[l] < self.keep_prob
                     A[l] *= D[l]/self.keep_prob
 
-        return Z, A, D
+        return A, D
 
-    def calculate_cost(self, data: DataSet):
+    def calculate_cost(self, data: DataSet, A, W):
         L = len(self.neuron_dims)
         X, Y = data.get_data()
         m = data.get_m()
-        J = (-1 / m) * (np.dot(Y.T, np.log(self.A[L])) + np.dot((1 - Y).T, np.log(1 - self.A[L]))).squeeze()
+        J = (-1 / m) * (np.dot(Y.T, np.log(A[L])) + np.dot((1 - Y).T, np.log(1 - A[L]))).squeeze()
 
         if self.lam != 0:
             for l in range(1, L + 1):
-                J += (self.lam/(2*m))*np.linalg.norm(self.W[l])**2
+                J += (self.lam/(2*m))*np.linalg.norm(W[l])**2
 
         return J
 
@@ -131,61 +134,120 @@ class NeuralNet:
         # Should add an exception if the activation isn't an anticipated value
         return dg
 
-    def backward_prop(self, data: DataSet):
+    def test_grads(self, data: DataSet, W, b, dW, db, epsilon):
+        L = len(self.neuron_dims)
+
+        if L > 1:
+            min_length = L-1
+        else:
+            min_length = L
+
+        dW_test = [None] * (L+1)
+        for l in range(min_length, L + 1):
+            dW_test[l] = np.zeros(W[l].shape)
+            for r in range(W[l].shape[0]):
+                for c in range(W[l].shape[1]):
+                    W_plus_test = copy.deepcopy(W)
+                    W_plus_test[l][r,c] += epsilon
+                    A, D = self.forward_prop(data, W_plus_test, b)
+                    J_plus = self.calculate_cost(data, A, W_plus_test)
+
+                    W_minus_test = copy.deepcopy(W)
+                    W_minus_test[l][r, c] -= epsilon
+                    A, D = self.forward_prop(data, W_minus_test, b)
+                    J_minus = self.calculate_cost(data, A, W_minus_test)
+
+                    dW_test[l][r, c] = (J_plus - J_minus) / (2*epsilon)
+            dW_test[l] = dW_test[l].T
+            dW_delta = np.linalg.norm(dW[l] - dW_test[l]) / (np.linalg.norm(dW[l]) + np.linalg.norm(dW_test[l]))
+            if dW_delta > epsilon:
+                print("GRAD FAIL: dW_delta=" + str(dW_delta))
+            else:
+                print("GRAD PASS: dW_delta=" + str(dW_delta))
+
+        db_test = [None] * (L+1)
+        for l in range(min_length, L + 1):
+            db_test[l] = np.zeros(b[l].shape)
+            for r in range(b[l].shape[0]):
+                for c in range(b[l].shape[1]):
+                    b_plus_test = copy.deepcopy(b)
+                    b_plus_test[l][r,c] += epsilon
+                    A, D = self.forward_prop(data, W, b_plus_test)
+                    J_plus = self.calculate_cost(data, A, W)
+
+                    b_minus_test = copy.deepcopy(b)
+                    b_minus_test[l][r, c] -= epsilon
+                    A, D = self.forward_prop(data, W, b_minus_test)
+                    J_minus = self.calculate_cost(data, A, W)
+
+                    db_test[l][r, c] = (J_plus - J_minus) / (2*epsilon)
+            db_test[l] = db_test[l].T
+            db_delta = np.linalg.norm(db[l] - db_test[l]) / (np.linalg.norm(db[l]) + np.linalg.norm(db_test[l]))
+            if db_delta > epsilon:
+                print("GRAD FAIL: db_delta=" + str(db_delta))
+            else:
+                print("GRAD PASS: db_delta=" + str(db_delta))
+
+
+    def backward_prop(self, data: DataSet, A, D, W, b):
         L = len(self.neuron_dims)
         m = data.get_m()
         X, Y = data.get_data()
-        dA = (-1/m)*np.divide((Y-self.A[L]), self.A[L]*(1-self.A[L])).T
-        dg = NeuralNet.d_g(self.final_activation, self.A[L].T)
+        dA = (-1/m)*np.divide((Y-A[L]), A[L]*(1-A[L])).T
+        dg = NeuralNet.d_g(self.final_activation, A[L].T)
         dZ = [dA*dg, ]
         db = [np.sum(dZ[0], axis=1, keepdims=True), ]
-        dW = [np.dot(dZ[0], self.A[L-1]), ]
+        dW = [np.dot(dZ[0], A[L-1]), ]
 
         if self.lam != 0:
-            dW[0] += (self.lam/m)*self.W[L].T
+            dW[0] += (self.lam/m)*W[L].T
 
         for l in reversed(range(1, L)):
-            dg = NeuralNet.d_g(self.mid_activation, self.A[l].T)
-            dZ.insert(0, np.dot(self.W[l+1], dZ[0])*dg)
+            dg = NeuralNet.d_g(self.mid_activation, A[l].T)
+            dZ.insert(0, np.dot(W[l+1], dZ[0])*dg)
 
             if 0 < self.keep_prob < 1:
-                dZ[0] *= self.D[l].T/self.keep_prob
+                dZ[0] *= D[l].T/self.keep_prob
 
             db.insert(0, np.sum(dZ[0], axis=1, keepdims=True))
-            dW.insert(0, np.dot(dZ[0], self.A[l-1]))
+            dW.insert(0, np.dot(dZ[0], A[l-1]))
 
             if self.lam != 0:
-                dW[0] += (self.lam / m) * self.W[l].T
+                dW[0] += (self.lam / m) * W[l].T
 
-        dZ.insert(0, None)
         dW.insert(0, None)
         db.insert(0, None)
-        return dZ, dW, db
+        return dW, db
 
-    def update_parameters(self, rate):
+    def update_parameters(self, rate, dW, db):
         L = len(self.neuron_dims)
         for l in range(1, L+1):
-            self.W[l] -= self.dW[l].T*rate
-            self.b[l] -= self.db[l].T*rate
+            self.W[l] -= dW[l].T*rate
+            self.b[l] -= db[l].T*rate
 
     def train(self, data: DataSet, iterations: int=1000, rate: float=0.0075, epsilon: float=0):
         old_keep_prob = self.keep_prob
         self.keep_prob = 1
         n = data.get_n()
-        self.initialize_parameters(n)
+        self.W, self.b = self.initialize_parameters(n)
 
         for i in range(iterations):
-            self.Z, self.A, self.D = self.forward_prop(data)
-            self.J = self.calculate_cost(data)
-            print(str(i) + ": " + str(self.J))
-            self.dZ, self.dW, self.db = self.backward_prop(data)
-            self.update_parameters(rate)
+            A, D = self.forward_prop(data, self.W, self.b)
+            J = self.calculate_cost(data, A, self.W)
+
+            dW, db = self.backward_prop(data, A, D, self.W, self.b)
+
+            if epsilon != 0:
+                self.test_grads(data, self.W, self.b, dW, db, epsilon)
+
+            print(str(i) + ": " + str(J))
+            self.update_parameters(rate, dW, db)
 
         self.keep_prob = old_keep_prob
 
     def test(self, data: DataSet):
         X, Y = data.get_data()
-        Z, A, D = self.forward_prop(data)
+        A, D = self.forward_prop(data, self.W, self.b)
         L = len(self.neuron_dims)
         Y_hat = np.round(A[L])
         return 1 - np.sum(np.abs(Y_hat - Y), axis=0).squeeze()/Y.size
@@ -209,7 +271,7 @@ train_data = DataSet(train_set_x_orig, train_set_y_orig)
 test_data = DataSet(test_set_x_orig, test_set_y_orig)
 
 nn = NeuralNet((50,25,10,1), lam=1, keep_prob=1)
-nn.train(train_data, iterations=2500, rate=0.0075, epsilon=1e-7)
+nn.train(train_data, iterations=2500, rate=0.0075) #, epsilon=1e-7
 
 print(nn.test(train_data))
 print(nn.test(test_data))
